77926360de5a4b428d5f8fb666d64ad21155353a
Gopal Khichar <gopalkh@iprogrammer.com>
2021-10-11 10:45:40 +0530

    ui for expense tracker

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Card, CardHeader, CardContent, Typography, Grid, Divider, } from '@material-ui/core';
+
+import useStyles from './styles';
+import Form from './Form/Form';
+import List from './List/List';
+
+const Main = () => {
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.root}>
+            <CardHeader title="Expense Tracker" subheader="Powered by Speechly..." />
+            <CardContent>
+                <Typography align="center" variant="h5" >Total Balance $100</Typography>
+                <Typography variant="subtitle2" style={{ lineHeight: '1.5rem', marginTop: '20px'}}>
+                    {/* info card */}
+                    Try Saying: Add income for $100 in Category Salary for monday...
+                </Typography>
+                <Divider />
+                {/* Form */}
+                <Form />
+            </CardContent>
+            <CardContent className={classes.cartContent}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <List />
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Main
